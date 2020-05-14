396d5a75d2e13e6d33514739472b8f39cbdd38e5
oguzhancilenk <oguzhancilenk@hotmail.com>
2020-05-14 16:16:16 +0300

    Geo App eklendi

--- todo/views.py
+++ todo/views.py
@@ -3,14 +3,20 @@ from django.views.decorators.csrf import csrf_exempt
 from django.utils import timezone
 from todo.models import Todo
 from django.http import HttpResponseRedirect
-
+import requests
 
 # Create your views here.
 
 def index(request):
+    response = requests.get("http://api.ipstack.com/149.140.187.82?access_key=2f8b706956c38ab53738b8c285cb73f4&format=1")
+    geodata = response.json()
+
     todo_items = Todo.objects.all().order_by("-added_date")
     return render(request, "main/index.html", {
-        "todo_items" : todo_items
+        "todo_items" : todo_items,
+ 
+        "ip" : geodata["ip"],
+        "city": geodata["city"],
     })
 
 @csrf_exempt
